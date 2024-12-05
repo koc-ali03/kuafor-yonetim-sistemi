263fd9411991f6b9be63f5ab306f7eb2f6823315
Bedirhan Can <bedirhancan2004@gmail.com>
2024-12-05 09:42:43 +0300

    - _Layout.cshtml'deki sidebar ve footer güncellendi. - Home/Index'e eklemeler yapıldı.

--- src/KuaforSalonu/obj/Release/net8.0/KuaforSalonu.AssemblyInfo.cs
+++ src/KuaforSalonu/obj/Release/net8.0/KuaforSalonu.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("KuaforSalonu")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcc8a15ee9ce91b507106b6f87910851236d90b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("KuaforSalonu")]
