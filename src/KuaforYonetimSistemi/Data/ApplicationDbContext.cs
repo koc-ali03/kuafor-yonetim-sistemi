101c39601914feafda702c21f56143d202362c31
Ali <mail@aliko.cc>
2024-12-14 23:53:36 +0300

    Register/Login
    - Kullanıcı kaydı ve giriş yapma özelliği eklendi.

---
+++ src/KuaforYonetimSistemi/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using KuaforYonetimSistemi.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace KuaforYonetimSistemi.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Salon> Salons { get; set; }
+        public DbSet<Calisan> Calisanlar { get; set; }
+        public DbSet<Servis> Servisler { get; set; }
+    }
+}
